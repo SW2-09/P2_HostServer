4a5081306349f5091e796870d6bbb270c2e920b2
sebub <ssma22@student.aau.dk>
2023-04-11 12:06:36 +0200

    Changed workerfile

--- public/javascripts/worker.js
+++ public/javascripts/worker.js
@@ -1,22 +1,17 @@
-let intervalId = null;
-
-// initialize webSocket as ws
 let ws;
 let subtasks_completed = 0;
 
-// open ws connection and handler for "message" events
+// open ws connection and hand  er for "message" events
 function openWsConnection(){
   ws = new WebSocket("ws://localhost:8001");
   let workerID = Math.floor(Math.random() * 1000);
   ws.addEventListener("message", e=>{
-          console.log("Client recieved:"+e.data);
+          if(e.data === "0"){
+              console.log("Not work to do, waiting for new jobs");
+          }
+          else{
+          console.log(`You recieved task:\n`+e.data);
           let nextSubtask=JSON.parse(e.data);
-          console.log("Next subtask:");
-          console.log(nextSubtask);
-          console.log("Matrix A:");
-          console.log(nextSubtask.matrixA);
-          console.log("Matrix B:");
-          console.log(nextSubtask.matrixB);
 
           let alg=new Function('A','B',nextSubtask.alg);
 
@@ -40,9 +35,16 @@ function openWsConnection(){
           ws.send(JSON.stringify(subSolution));
          // updateUserTasksComputed();
           console.log(`A subsolution was send by worker: ${subSolution.workerID}`);
+        }
       }
   );
 return ws;
+}
+
+
+// Stops the websocket connection
+function stopWsConnection(ws){
+    ws.close();
 }
 
 //Ensure only 1 checkbox can be checked
@@ -57,15 +59,20 @@ checkboxes.forEach((checkbox) => {
   });
 });
 
+
 function handleChange(event) {
   const option = event.target.value;
   const checkboxes = document.getElementsByName("option");
 
   if (option === "yes" && event.target.checked) {
-  
+    openWsConnection();
+
     checkboxes[1].checked = false;
-  } else{
-  
+  } else if (option === "no" && event.target.checked) {
+
+    console.log("Worker is not computing");
+
+    stopWsConnection(ws);
     checkboxes[0].checked = false;
   } 
 }
